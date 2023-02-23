73aad085447344167767f4c51934a8b35b5825b7
marharita08 <rozgon.margarita08@gmail.com>
2023-02-23 11:24:06 +0200

    lab3

---
+++ lab3/HelloConstraint/app/src/main/java/com/example/android/helloconstraint/Color.java
@@ -0,0 +1,8 @@
+package com.example.android.helloconstraint;
+
+public class Color {
+    public static final int GREY = -8355712; // #808080
+    public static final int GREEN = -12806596; // #3c963c
+    public static final int YELLOW = -2972416; // #d2a500
+    public static final int PURPLE = -5621916; // #aa3764
+}
